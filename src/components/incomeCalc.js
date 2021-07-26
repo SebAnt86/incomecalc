ac6e01d955cf68b41d5f9d2b6d62e5c6b8f9ddd2
notevenagoat <itinoco@pacificstream.com.au>
2021-07-26 20:41:13 +1000

    imported tables into incomeCalc

--- src/components/incomeCalc.js
+++ src/components/incomeCalc.js
@@ -1,37 +1,15 @@
 
 // we should do all the calculations by week, then if timePeriod changes, we mutiply
-
 // this function should take:, userCat, userRate, userHours, and a default partnerIncome
+import {userCategories as userCat} from "./govTables.js";
 
-function incomeCalc() {
+function incomeCalc(userRate,userHours) {
 
-// to workout the logic, lets start with fake values
+// variables to be returned:
+let workIncomeTotal = userRate * userHours;
 
-let userCat = [{ // <-- using this as an example, but it should be read from userCat
-    catName : 'Single',
-    maxPayment : 620.80,
-    incomeLimit: 1217.00,
-    bracketOne : .50, // 50 cents for each dollar over $150 
-    bracketTwo : .60, // 60 cents for each dollar over $256 
-    
-},{ // <-- using this as an example, but it should be read from userCat
-    catName : 'Partnered',
-    maxPayment : 565.40,
-    incomeLimit: 1123.17,
-    categoryPartnerLimit: 2079.50,
-    bracketOne : .50, // 50 cents for each dollar over $150 
-    bracketTwo : .60, // 60 cents for each dollar over $256 
-    
-}];
-
-let userRate = 50;
-let userHours= 6; 
-// defining the variables to be returned:
-
-let totalIncome = 0; //total of (totalHours x userRate ) + (maxPayment - deductions) 
 let deductions = 0;
 
-let workIncomeTotal = userRate * userHours;
 
 // 300  (106 * limit1) ( 150-256)
 let threshold1 =  ((workIncomeTotal - 150) * userCat[0].bracketOne).toFixed(2);// how much money he made between 150 to 256
@@ -56,4 +34,4 @@ console.log(`User worked ${workIncomeTotal} , was elegible for ${userCat[0].maxP
 console.log(totalIncome);
 }
 
-incomeCalc();
+incomeCalc(50,3);
