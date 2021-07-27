46df6b24772d8b9cd47ff25a157c861a1e928eb5
Antonio Sebastiano <sebastianoa@hotmail.com>
2021-07-27 19:25:16 +1000

    round the result to two decimals

--- src/components/Result.js
+++ src/components/Result.js
@@ -12,7 +12,7 @@ function Result(props) {
         <p> Income from working: {result.workHours} hours is {result.workIncomeTotal} </p>
         <p> The goverment payment max you could receive was {result.maxGovPayment} </p>
         <p> Gov payment deducted by:  {result.deductions} </p>
-        <p> Final Gov payment received:  {result.maxGovPayment - result.deductions} </p>
+        <p> Final Gov payment received:  {(result.maxGovPayment - result.deductions).toFixed(2)} </p>
         <p> Total income  is {result.totalIncome} </p>
      
         {/* <p> Your average wage after deduction is  {result.averageWage} </p> */}
--- src/components/incomeCalc.js
+++ src/components/incomeCalc.js
@@ -6,49 +6,39 @@ export const incomeCalc = (userRate, userHours =1, userCategories) => {
   let workIncomeTotal = userRate * userHours;
   let deductions = 0;
 
-  let threshold1 = (
-    (workIncomeTotal - 150) *
-    userCategories.bracketOne
-  ).toFixed(2); // how much money he made between 150 to 256
+  let threshold1 = ((workIncomeTotal - 150) * userCategories.bracketOne).toFixed(2); // how much money he made between 150 to 256
   let threshold2 = 0; // between 256 to max
 
   if (workIncomeTotal > userCategories.incomeLimit) {
     //<-- if total income higher than limit
     deductions = userCategories.maxPayment;
   } else if (workIncomeTotal < userCategories.incomeLimit) {
+      // deduction from 0 to 150
     if (workIncomeTotal < 150) {
       deductions = 0;
     } else if (workIncomeTotal > 150 && workIncomeTotal < 257) {
       // deduction from 150 to 256
       deductions = threshold1;
-    } else if (
-      workIncomeTotal > 256 &&
-      workIncomeTotal < userCategories.incomeLimit
-    ) {
+    } else if (workIncomeTotal > 256 && workIncomeTotal < userCategories.incomeLimit) {
       // deduction from 256 to incomeLimit
-      //console.log("256 to limit");
-      threshold2 = (
-        (workIncomeTotal - 150 - 106) *
-        userCategories.bracketTwo
-      ).toFixed(2);
-      deductions =
-        workIncomeTotal - 150 - 106 * userCategories.bracketOne - threshold2;
+      threshold2 = ((workIncomeTotal - 150 - 106) * userCategories.bracketTwo).toFixed(2);
+      deductions = ((workIncomeTotal - 150) - (106 * userCategories.bracketOne) - threshold2).toFixed(2);
     }
   }
-  let totalIncome = parseFloat(
-    (workIncomeTotal + userCategories.maxPayment - deductions).toFixed(2));
+  let totalIncome = (workIncomeTotal + userCategories.maxPayment - deductions).toFixed(2);
       
   let averageWage = Math.round(totalIncome/userHours);
-  deductions = parseFloat(deductions);
+  
 
   let calculatedDisplay = {
     totalIncome,
     workIncomeTotal,
     deductions,
-    maxGovPayment: userCategories.maxPayment,
+    maxGovPayment: (userCategories.maxPayment).toFixed(2),
     averageWage,
     workHours : userHours,
   };
 
+  console.log(calculatedDisplay.deductions);
   return calculatedDisplay;
 };
