0f0eea27c9c404dd169569863fbaf66544c079e6
Bhagyashree <bhagya099@gmail.com>
2021-07-29 15:40:04 +1000

    added Table component in result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,8 +1,16 @@
 import React from "react";
-
+import { Table, TableRow ,TableCell,TableBody } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
 
 function Result(props) {
   const { result } = props;
+  const classes = useStyles();
+  // const classes = useStyles();
   // if (!result) {
   //   return <div> EMPTY </div>;
   // }
@@ -11,15 +19,43 @@ function Result(props) {
   return (
     <div className="result">
       <h2 className="result_heading"> RESULT </h2>
-      <div>
-        <p> Income from working: {result.workHours} hours is ${result.workIncomeTotal} </p>
-        <p> The goverment payment max you could receive was ${result.maxGovPayment} </p>
-        <p> Gov payment deducted by:  ${result.deductions} </p>
-        <p> Final Gov payment received:  ${finalGovPay} </p>
-        <p> Total income  is ${result.totalIncome} </p>
+      {/* <TableContainer component={Paper}> */}
+      <Table className={classes.table}  aria-label="simple table">
+          {/* <div> */}
+        <TableBody>
+          <TableRow>
+             <TableCell>Income from working:</TableCell>
+              <TableCell align="right">
+                {result.workHours} hours is ${result.workIncomeTotal}
+              </TableCell>
+            </TableRow>
+          <TableRow>
+            <TableCell>The goverment payment max you could receive was</TableCell>
+            <TableCell align="right">
+                ${result.maxGovPayment}
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell> Gov payment deducted by:
+            </TableCell>
+            <TableCell align="right">
+              ${result.deductions}
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell> Final Gov payment received:</TableCell>
+            <TableCell align="right">
+              ${finalGovPay}</TableCell>
+          </TableRow>
+          <TableRow><TableCell> Total income  is:</TableCell>
+            <TableCell align="right">${result.totalIncome}</TableCell>
+          </TableRow>
      
-        {/* <p> Your average wage after deduction is  {result.averageWage} </p> */}
-      </div>
+            {/* <p> Your average wage after deduction is  {result.averageWage} </p> */}
+        </TableBody>
+      </Table>
+        {/* </TableContainer> */}
+        {/* </div> */}
     </div>
   );
 }
